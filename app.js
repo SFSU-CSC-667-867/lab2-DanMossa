a4e37e1818c421a0e39943b963eca6fde08bd2c7
Daniel Mossaband <Daniel@Mossaband.com>
2019-02-13 20:23:49 -0800

    app.js with routes

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
-console.log('Hello World');
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/hello', (req, res) => {
+    console.log('Hello ' + req.query.username);
+    res.send('world')
+});
+app.get('/add', (req, res) => res.send('0'));
+
+app.listen(port, () => {
+    return console.log(`Example app listening on port ${port}!`);
+})

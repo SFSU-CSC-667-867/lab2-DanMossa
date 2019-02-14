8b145cf13e7be748efba79dfc2bb1de777293ac7
Daniel Mossaband <Daniel@Mossaband.com>
2019-02-13 20:50:21 -0800

    Finished demo

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const axios = require('axios');
 const app = express();
 const port = 3000;
 
@@ -6,6 +7,19 @@ app.get('/hello', (req, res) => {
     console.log(req.query);
     res.send('Hello ' + req.query.username);
 });
+
+app.get('/network', (req, res) => {
+    axios.get('http://localhost:3001/world')
+        .then((networkResponse) => {
+            res.send(networkResponse.data);
+
+        })
+        .catch(() => {
+            res.send(':( did not work.');
+        });
+});
+
+
 app.get('/add', (req, res) => res.send('0'));
 
 app.listen(port, () => {
